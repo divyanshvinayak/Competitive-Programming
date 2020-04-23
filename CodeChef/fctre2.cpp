c4bb1df61807269b814fbbafa30b1dacc5403802
Divyansh Vinayak <b19080@students.iitmandi.ac.in>
2020-04-24 00:09:07 +0530

    Add APRIL20B

--- CodeChef/fctre2.cpp
+++ CodeChef/fctre2.cpp
@@ -1,8 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
-#pragma GCC optimization("Ofast")
-#pragma GCC optimization("unroll-loops")
 
 typedef long long ll;
 typedef unsigned long long ull;
@@ -32,10 +29,10 @@ typedef unordered_set<ll> us;
 #define pb emplace_back
 #define all(v) v.begin(), v.end()
 #define allr(arr, r) arr, arr + r
-#define showv(v) for(auto it: v) cout<<it.F<<':'<<it.S<<' '; ln;
+#define showv(v) for(auto it: v) cout<<it<<' '; ln;
 #define show2d(v) for(auto it: v) {for(auto j: it) cout<<j<<' '; ln;}
 #define printv(v) for(auto it: v) cout<<it<<endl;
-#define showvm(v) for(auto it: v) {for(auto j: it) cout<<j.F<<':'<<j.S<<' '; ln;}
+#define showvm(v) for(auto it: v) cout<<it.F<<':'<<it.S<<' '; ln;
 #define sortv(v) sort(all(v));
 #define revv(v) reverse(all(v));
 #define maxv(v) *max_element(all(v))
@@ -52,20 +49,24 @@ typedef unordered_set<ll> us;
 #define sz(a) a.size()
 #define precise(x) cout<<fixed<<setprecision(x);
 
-const ll MAXN = 1e6+5, MOD = 1e9+7, SIZE = 2e5+5, INF = 0x7f7f7f7f7f7f7f7f;
+const ll MAXN = 1e6+5, MOD = 1e9+7, SIZE = 2e5+5, BLOCKSIZE = 1e3, LN = 19, INF = 0x7f7f7f7f7f7f7f7f;
 const int INFi = 0x7f7f7f7f;
 const double EPS = 1e-9, PI = acos(-1);
 
 //<<------Declare Variables Here----->>//
-ll t, n, x, y, ans, q, lca, val, ptr, d, dx, i, j, m, min_f, max_x, max_y, FAI[SIZE], dp[SIZE][19], logn[SIZE], level[SIZE];
-vll spf(MAXN, 2), pow2(20, 1), euler, depthArr;
+ll t, n, x, y, q, lca, val, ptr, d, dx, min_f, max_x, max_y, ans, L, R, fai[SIZE], lai[SIZE], dp[SIZE << 1][LN], logn[SIZE], visited[SIZE], counter[MAXN], res[SIZE], id[SIZE << 1], inv[LN * SIZE], euler[SIZE << 1], depth[SIZE << 1];
+vll spf(MAXN, 2), pow2(LN, 1);
 vvll v;
-vvpll a, z;
-vpll counter;
+vpll factors, a[SIZE];
+struct query_t
+{
+	ll left, right, lca, id;
+} queries[SIZE];
 
 //<<-----Implement Functions Here---->>//
-void sieve()
+void precompute()
 {
+    val = 1, ptr = 0, inv[0] = 1, inv[1] = 1;
     FORI(i, 3, MAXN, 2)
         spf[i] = i;
     FORI(i, 3, sqrt(MAXN), 2)
@@ -73,19 +74,24 @@ void sieve()
             FORI(j, i*i, MAXN, i)
                 if(spf[j]==j)
                     spf[j] = i;
-}
-
-void precompute()
-{
-    val = 1, ptr = 0;
-    FOR(i, 1, 20)
-        pow2[i] = pow2[i-1]*2;
+    FOR(i, 1, LN)
+        pow2[i] = pow2[i-1] * 2;
     FOR(i, 1, SIZE)
     {
         logn[i] = ptr-1;
-        if(i == val)
+        if(i==val)
             val *= 2, logn[i] = ptr, ptr++;
     }
+    FOR(i, 2, LN*SIZE)
+        inv[i] = (inv[MOD % i] * (MOD - MOD / i)) % MOD;
+}
+
+void initialize()
+{
+    ptr = 0;
+    v = vvll(n+1);
+    reset(counter, 0);
+    reset(visited, 0);
 }
 
 void addEdge(ll x, ll y)
@@ -96,80 +102,45 @@ void addEdge(ll x, ll y)
 
 vpll getPrimeFactorization(ll x)
 {
-    min_f = 1, counter = {};
+    min_f = 1, factors = {};
     while(x>1)
     {
         y = spf[x], x /= y;
         if(y>min_f)
-            counter.pb(y, 1), min_f = y;
+            factors.pb(y, 1), min_f = y;
         else
-            counter.back().S++;
+            factors.back().S++;
     }
-    return counter;
-}
-
-vpll merge(vpll x, vpll y)
-{
-    i = 0, j = 0, max_x = sz(x), max_y = sz(y), counter = {};
-    while(i < max_x && j < max_y)
-        if(x[i].F==y[j].F)
-            counter.pb(x[i].F, x[i].S+y[j].S), i++, j++;
-        else if(x[i].F<y[j].F)
-            counter.pb(x[i]), i++;
-        else
-            counter.pb(y[j]), j++;
-    while(i < max_x)
-        counter.pb(x[i]), i++;
-    while(j < max_y)
-        counter.pb(y[j]), j++;
-    return counter;
-}
-
-vpll difference(vpll x, vpll y)
-{
-    i = 0, j = 0, max_x = sz(x), max_y = sz(y), counter = {};
-    while(j < max_y)
-        if(x[i].F==y[j].F)
-            counter.pb(x[i].F, x[i].S-y[j].S), i++, j++;
-        else
-            counter.pb(x[i]), i++;
-    while(i < max_x)
-        counter.pb(x[i]), i++;
-    return counter;
+    return factors;
 }
 
 void DFS(ll cur, ll prev, ll dep)
 {
-    if(FAI[cur] == -1)
-        FAI[cur] = ptr;
-    level[cur] = dep;
-    euler.pb(cur);
+    fai[cur] = ptr;
+    id[ptr] = cur;
+    euler[ptr] = cur;
+	depth[ptr] = dep;
     ptr++;
     FORALL(x, v[cur])
-        if(x != prev)
+        if(x!=prev)
         {
-            z[x] = merge(z[cur], a[x]);
             DFS(x, cur, dep+1);
-            euler.pb(cur);
+            euler[ptr] = cur;
+			depth[ptr] = dep;
             ptr++;
         }
-}
-
-void createDepthArr()
-{
-    FORALL(x, euler)
-        depthArr.pb(level[x]);
+    id[ptr] = cur;
+	lai[cur] = ptr;
 }
 
 void buildSparseTable()
 {
-    x = sz(depthArr);
-    FOR(i, 1, x)
-        dp[i-1][0] = (depthArr[i]>depthArr[i-1])?i-1:i;
-    FOR(l, 1, 19)
-        FOR(i, 0, x)
+    FOR(i, 1, 2*n)
+        dp[i-1][0] = (depth[i]>depth[i-1])?i-1:i;
+    FOR(l, 1, LN)
+        FOR(i, 0, 2*n)
             if(dp[i][l-1]!=-1 && dp[i+pow2[l-1]][l-1]!=-1)
-                dp[i][l] = (depthArr[dp[i][l-1]]>depthArr[dp[i+pow2[l-1]][l-1]])?dp[i+pow2[l-1]][l-1]:dp[i][l-1];
+                dp[i][l] = (depth[dp[i][l-1]]>depth[dp[i+pow2[l-1]][l-1]])?dp[i+pow2[l-1]][l-1]:dp[i][l-1];
             else
                 break;
 }
@@ -179,7 +150,7 @@ ll query(ll l, ll r)
     d = r-l, dx = logn[d];
     if(l==r)
         return l;
-    if(depthArr[dp[l][dx]] > depthArr[dp[r-pow2[dx]][dx]])
+    if(depth[dp[l][dx]] > depth[dp[r-pow2[dx]][dx]])
         return dp[r-pow2[dx]][dx];
     else
         return dp[l][dx];
@@ -189,22 +160,80 @@ ll LCA(ll u, ll v)
 {
     if(u==v)
         return u;
-    if(FAI[u]>FAI[v])
-       swap(u, v);
-    return euler[query(FAI[u], FAI[v])];
+    return euler[query(fai[u], fai[v])];
+}
+
+bool compare(const query_t &a,const query_t &b)
+{
+    return a.left/BLOCKSIZE!=b.left/BLOCKSIZE?a.left<b.left:a.right<b.right;
+}
+
+void add(vpll v)
+{
+    FORALL(it, v)
+        ans = (ans*inv[counter[it.F]+1])%MOD, counter[it.F]+=it.S, ans = (ans*(counter[it.F]+1))%MOD;
+}
+
+void remove(vpll v)
+{
+    FORALL(it, v)
+        ans = (ans*inv[counter[it.F]+1])%MOD, counter[it.F]-=it.S, ans = (ans*(counter[it.F]+1))%MOD;
+}
+
+void verify(ll x)
+{
+    if(visited[x])
+		remove(a[x]);
+	else
+		add(a[x]);
+	visited[x] ^= 1;
+}
+
+void getQueries()
+{
+	cin>>q;
+	FOR(i, 0, q)
+	{
+		cin>>x>>y;
+		if(fai[x]>fai[y])
+			swap(x, y);
+		lca = LCA(x, y);
+		if(lca==x)
+			queries[i] = {fai[x], fai[y], lca, i};
+		else
+			queries[i] = {lai[x], fai[y], lca, i};
+	}
+}
+
+void processQueries()
+{
+	sort(queries, queries+q, compare);
+	L = queries[0].left, R = queries[0].left - 1, ans = 1;
+	FOR(i, 0, q)
+	{
+		while(L<queries[i].left) verify(id[L++]);
+		while(L>queries[i].left) verify(id[--L]);
+		while(R<queries[i].right) verify(id[++R]);
+		while(R>queries[i].right) verify(id[R--]);
+		if(queries[i].lca!=id[L] && queries[i].lca!=id[R])
+			verify(queries[i].lca), res[queries[i].id]=ans, verify(queries[i].lca);
+		else
+			res[queries[i].id] = ans;
+	}
+	FOR(i, 0, q)
+		print(res[i]);
 }
 
 //<<----------Start of Main---------->>//
 int main()
 {
     fastio;
-    sieve();
     precompute();
     cin>>t;
     while(t--)
     {
         cin>>n;
-        ptr = 0, v = vvll(n+1), a = vvpll(n+1), euler = depthArr = {}, reset(FAI, -1), reset(dp, -1);
+        initialize();
         FOR(i, 1, n)
         {
             cin>>x>>y;
@@ -215,19 +244,10 @@ int main()
             cin>>x;
             a[i] = getPrimeFactorization(x);
         }
-        z = a;
         DFS(1, 0, 0);
-        createDepthArr();
         buildSparseTable();
-        cin>>q;
-        FOR(i, 0, q)
-        {
-            cin>>x>>y;
-            ans = 1, lca = LCA(x, y);
-            FORALL(it, merge(merge(difference(z[x], z[lca]), difference(z[y], z[lca])), a[lca]))
-                ans=(ans*(it.S+1))%MOD;
-            print(ans);
-        }
+        getQueries();
+		processQueries();
     }
     return 0;
 }
